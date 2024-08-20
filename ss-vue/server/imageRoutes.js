fd1460cec94911864c5489cbef062d6f0e931749
Takemyeye <mellovan2005@gmail.com>
2024-08-20 04:14:58 +0200

    server

---
+++ ss-vue/server/imageRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const artData = require('./data/artData');
+
+router.get('/images', (req, res) => {
+  const category = req.query.category;
+  
+  try {
+    let images;
+
+    if (category && artData[category]) {
+      images = artData[category].map(item => ({
+        id: item.id,
+        title: item.title,
+        image: item.image,
+        price: item.price,
+        subtitle: item.subtitle
+      }));
+    } else {
+      images = artData.all.map(item => ({
+        id: item.id,
+        title: item.title,
+        image: item.image,
+        price: item.price,
+        subtitle: item.subtitle
+      }));
+    }
+    
+    res.json(images);
+  } catch (error) {
+    console.error('Error fetching images data:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
--- ss-vue/server/server.js
+++ ss-vue/server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const artData = require('./data/artData');
+const imageRoutes = require('./imageRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,38 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use('/art', express.static(path.join(__dirname, '../dist/art')));
 
-app.get('/api/images', (req, res) => {
-  const category = req.query.category;
-  
-  try {
-    let images;
-
-    if (category && artData[category]) {
-      // Возвращаем изображения для выбранной категории
-      images = artData[category].map(item => ({
-        id: item.id,
-        title: item.title,
-        image: item.image,
-        price: item.price,
-        subtitle: item.subtitle
-      }));
-    } else {
-      // Возвращаем все изображения, если категория не указана или не существует
-      images = artData.all.map(item => ({
-        id: item.id,
-        title: item.title,
-        image: item.image,
-        price: item.price,
-        subtitle: item.subtitle
-      }));
-    }
-    
-    res.json(images);
-  } catch (error) {
-    console.error('Error fetching images data:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+app.use('/api', imageRoutes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
