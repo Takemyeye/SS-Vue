f1d5a73d50776e9bc267102e7ea449a33a13082f
Takemyeye <mellovan2005@gmail.com>
2024-09-18 21:42:31 +0200

    doun

--- src/services/unit/messengBlock.vue
+++ src/services/unit/messengBlock.vue
@@ -58,7 +58,7 @@
         const token = localStorage.getItem('token');
 
         try {
-            const response = await fetch('http://localhost:3000/api/messages/send', {
+            const response = await fetch('http://localhost:3000/api/messages', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
