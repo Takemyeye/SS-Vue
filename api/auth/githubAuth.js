80b6b0ced6582f612e13606267e55b778fb5155c
Takemyeye <mellovan2005@gmail.com>
2024-09-23 22:03:21 +0200

    \

--- api/auth/githubAuth.js
+++ api/auth/githubAuth.js
@@ -30,9 +30,9 @@ router.get('/auth/github/callback', passport.authenticate('github', { session: f
     });
 
     await newUser.save();
-    res.redirect(`http://localhost:8080?token=${token}`);
+    res.redirect(`https://soulswap.netlify.app?token=${token}`);
   } else {
-    res.redirect(`http://localhost:8080?token=${existingUser.token}`);
+    res.redirect(`https://soulswap.netlify.app?token=${existingUser.token}`);
   }
 });
 
