e20e6c0829148f8363fa94dc57f83420d68ab9cb
Takemyeye <mellovan2005@gmail.com>
2024-09-24 22:31:42 +0200

    yes

--- api/auth/googleAuth.js
+++ api/auth/googleAuth.js
@@ -34,9 +34,9 @@ router.get('/auth/google/callback', passport.authenticate('google', { session: f
     });
 
     await newUser.save();
-    res.redirect(`http://localhost:8080?token=${token}`);
+    res.redirect(`https://ss-vue-yf04.onrender.com?token=${token}`);
   } else {
-    res.redirect(`http://localhost:8080?token=${existingUser.token}`);
+    res.redirect(`https://ss-vue-yf04.onrender.com?token=${existingUser.token}`);
   }
 });
 
