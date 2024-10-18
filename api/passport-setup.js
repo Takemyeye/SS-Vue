0388980f3d8f1bdb9880bb3468cfaddb38fbf935
Takemyeye <mellovan2005@gmail.com>
2024-10-18 19:34:38 +0200

    nginx

--- api/passport-setup.js
+++ api/passport-setup.js
@@ -1,7 +1,7 @@
-const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const GitHubStrategy = require('passport-github2').Strategy;
 const DiscordStrategy = require('passport-discord').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
 require('dotenv').config();
 
 passport.use(new GoogleStrategy({
