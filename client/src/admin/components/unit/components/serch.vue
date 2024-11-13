82bd7e07552068b9ee2c9ca552fceadf29c1fb9e
Takemyeye <mellovan2005@gmail.com>
2024-11-13 20:12:20 +0100

    serch admin

---
+++ client/src/admin/components/unit/components/serch.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="group">
+    <font-awesome-icon icon="fa-solid fa-magnifying-glass" class="icon"/>
+    <input placeholder="Search" type="search" class="input">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SerchAdmin'
+  }
+</script>
+
+<style scoped>
+.group {
+ display: flex;
+ line-height: 28px;
+ align-items: center;
+ position: relative;
+ max-width: 190px;
+}
+
+.input {
+ width: 100%;
+ height: 40px;
+ line-height: 28px;
+ padding: 0 1rem;
+ padding-left: 2.5rem;
+ border: 2px solid transparent;
+ border-radius: 8px;
+ outline: none;
+ background-color: #f3f3f4;
+ color: #0d0c22;
+ transition: .3s ease;
+}
+
+.input::placeholder {
+ color: #9e9ea7;
+}
+
+.input:focus, input:hover {
+ outline: none;
+ border-color: rgba(234,76,137,0.4);
+ background-color: #fff;
+ box-shadow: 0 0 0 4px rgb(234 76 137 / 10%);
+}
+
+.icon {
+  position: absolute;
+  left: 1rem;
+  color: #9e9ea7;
+  font-size: 1rem;
+}
+</style>
