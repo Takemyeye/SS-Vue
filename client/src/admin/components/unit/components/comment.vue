576f351079b152a4f546eacf8123cd2ac9582277
Takemyeye <mellovan2005@gmail.com>
2024-11-26 11:21:13 +0100

    comment block

--- client/src/admin/components/unit/components/comment.vue
+++ client/src/admin/components/unit/components/comment.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="">
-    <h1>Comments</h1>
+  <div class="main-block">
+    <div class="wrapper">
+      <div class="text">
+        <h2>Comment</h2>
+        <h5>Manage your store's products</h5>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -11,5 +17,41 @@
 </script>
 
 <style scoped>
+  .main-block {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    overflow: hidden;
+    gap: 2rem;
+  }
 
+  .wrapper {
+    width: 90%;
+    height: 80%;
+    padding: 2.5%;
+    background-color: white;
+    border: 1px solid rgba(221, 221, 221, 0.15);
+    border-radius: 8px;
+    overflow-y: auto;
+    scrollbar-width: 16px;
+    scrollbar-color: rgb(158, 158, 158) white;
+    scroll-behavior: smooth;
+  }
+
+  .container-btn {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  }
+
+  .text {
+    width: 100%;
+    display: flex;
+    align-items: start;
+    flex-direction: column;
+    gap: 1rem;
+  }
 </style>
