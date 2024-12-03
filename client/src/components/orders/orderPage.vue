2a13914952c2b6f02d9ae1262cf9bd438e788411
Takemyeye <mellovan2005@gmail.com>
2024-12-03 19:24:09 +0100

    .

--- client/src/components/orders/orderPage.vue
+++ client/src/components/orders/orderPage.vue
@@ -1,13 +1,29 @@
 <template>
-  <h1>Orders</h1>
+  <SiteHeader /> 
+  <div class="order">
+    <OrdersPanel />
+  </div>
 </template>
 
 <script>
+import OrdersPanel from './unit/ordersPanel.vue';
+import SiteHeader from '../header/header.vue';
+
   export default {
-    name: 'OrderPage'
+    name: 'OrderPage',
+    components: {
+      OrdersPanel,
+      SiteHeader,
+    }
   }
 </script>
 
 <style scoped>
-  
+  .order {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+  }
 </style>
---
+++ client/src/components/orders/unit/ordersPanel.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="orders">
+        <h1>ищу девушку</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OrdersPanel'
+    }
+</script>
+
+<style scoped>
+    .orders {
+        width: 80%;
+        padding: 2.5rem 0;
+    }
+</style>
